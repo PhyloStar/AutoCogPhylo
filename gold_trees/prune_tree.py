20d6063512583f9610ae107d486f1ce97d6fa47a
erathorn <wahle.joh@gmail.com>
2018-01-08 16:09:44 +0100

    inner_nodes are now present as leaves

--- gold_trees/prune_tree.py
+++ gold_trees/prune_tree.py
@@ -12,11 +12,12 @@ for fname in alles:
     tree_str = open(sys.argv[1]).read()
 
     tree_str = re.sub(r':\d', '',tree_str)
+    all_labels = tree_str.replace("(","").replace(")",",").split(",")
+
     leaves_list = []
     for line in open(fname, "r"):
         if "\t" in line:
             leaves_list.append(line.split("\t")[0])
-
 
     if "aa-" in family:
         leaves_list.remove("irrr1240")
@@ -25,6 +26,13 @@ for fname in alles:
             print(leaf)
     print(family, leaves_list, len(leaves_list))
     t = Tree(tree_str, format=1)
+    internals = set(leaves_list)-set([i.name for i in t.get_leaves()])
     t.prune(leaves_list)
-
+    all_leaves = [i.name for i in t.get_leaves()]
+    for iso in leaves_list:
+        if iso not in all_leaves:
+            target = t.search_nodes(name=iso)[0]
+            target.add_child(name=iso)
+            target.name = None
+    assert set([i.name for i in t.get_leaves()]) == set(leaves_list) # everything in leaves list is a leave in the tree
     t.write(format=9, outfile=filename+".glot_cleaned.tre")
